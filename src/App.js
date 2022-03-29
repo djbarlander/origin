eecb45c14fab336aa3d1617050962731248dd4e8
djbarlander <9800242@student.toiohomai.ac.nz>
2022-03-30 10:26:20 +1300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from './Header';
+import Footer from './Footer';
+import JSON from './data.json';
+
+const Data = JSON.map(
+
+        (data) => 
+        {
+            return(
+                    <div key={data.id}>
+                        <h4>{data.game}</h4>
+                        <p><img src={data.image} className="img-fluid" /></p>
+                        <p>{data.description}</p>
+                        <h5>{data.price}</h5>
+                    </div>
+            );
+        }
+
+);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <div className="p-3 shadow border row">
+        <div className="col-8">
+          {Data}
+        </div>
+      </div>
+      <Footer />
     </div>
   );
 }
---
+++ src/Footer.js
@@ -0,0 +1,12 @@
+function Footer()
+{
+    return(
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <span className="text-white">SCP Foundation 2022 </span>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
